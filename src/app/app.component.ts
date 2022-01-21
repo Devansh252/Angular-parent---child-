3ff2984b6e16b5fc1dc83d1fc3a50895f1481407
Devansh Shukla <shukladevansh007@gmail.com>
2022-01-21 11:19:36 +0530

    parent<->child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'childparent';
+  techlist = ['SQL', 'Web', 'APP'];
+  techList = ['SQL', 'Web', 'APP'];
+  addItem(val: any) {
+    this.techlist.push(val);
+  }
+  addItem2(val: any) {
+    this.techList.push(val);
+  }
 }
